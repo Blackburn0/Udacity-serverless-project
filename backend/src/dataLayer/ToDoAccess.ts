d8a2a8e7396d47e8384b8ad2caf9b5d68515aaa0
Blackburn0 <hamzatadebayo5@gmail.com>
2022-10-03 23:38:07 +0100

    first commit

---
+++ backend/src/dataLayer/ToDoAccess.ts
@@ -0,0 +1,105 @@
+import * as AWS from "aws-sdk";
+import { TodoUpdate } from "../models/TodoUpdate";
+import { Types } from 'aws-sdk/clients/s3';
+import { TodoItem } from "../models/TodoItem";
+import { DocumentClient } from "aws-sdk/clients/dynamodb";
+
+
+export class ToDoAccess {
+    constructor(
+        private readonly s3Client: Types = new AWS.S3({ signatureVersion: 'v4' }),
+        private readonly todoTable = process.env.TODOS_TABLE,
+        private readonly docClient: DocumentClient = new AWS.DynamoDB.DocumentClient(),
+        private readonly s3BucketName = process.env.S3_BUCKET_NAME) {
+    }
+
+    async getTodoForUser(userId: string): 
+    Promise<TodoItem[]> {
+        console.log
+            ("Fetching all todos");
+
+        const params = {
+            TableName: this.todoTable,
+            KeyConditionExpression: "#userId = :userId",
+            ExpressionAttributeNames: {"#userId": "userId"},
+            ExpressionAttributeValues: {":userId": userId}
+        };
+
+        const result = 
+            await this.docClient.query(params).promise();
+        console.log(result);
+        const items = result.Items;
+
+        return items as TodoItem[];
+    }
+
+    async createTodoForUser(todoItem: TodoItem): 
+        Promise<TodoItem> {
+        console.log
+            ("create a new todo");
+
+        const params = {TableName: this.todoTable, Item: todoItem,};
+
+        const result = 
+            await this.docClient.put(params).promise();
+        console.log(result);
+
+        return todoItem as TodoItem;
+    }
+
+    async updateTodoForUser(
+        todoUpdate: TodoUpdate, 
+        todoId: string, 
+        userId: string
+        ): Promise<TodoUpdate> {
+        console.log
+            ("Update existing todo");
+
+        const params = {
+            TableName: this.todoTable,
+            Key: {"userId": userId,"todoId": todoId},
+            UpdateExpression: "set #a = :a, #b = :b, #c = :c",
+            ExpressionAttributeNames: {"#a": "name","#b": "dueDate","#c": "done"},
+            ExpressionAttributeValues: {":a": todoUpdate['name'],":b": todoUpdate['dueDate'],":c": todoUpdate['done']},
+            ReturnValues: "ALL_NEW"
+        };
+
+        const result = 
+        await this.docClient.update(params).promise();
+        console.log(result);
+        const attributes = result.Attributes;
+
+        return attributes as TodoUpdate;
+    }
+
+    async deleteTodoForUser(
+        todoId: string, 
+        userId: string): 
+        Promise<string> {
+        console.log
+            ("delete an existing todo");
+
+        const params = {
+            TableName: this.todoTable,
+            Key: {"userId": userId,"todoId": todoId},
+        };
+
+        const result = 
+        await this.docClient.delete(params).promise();
+        console.log(result);
+
+        return "" as string;
+    }
+
+    async createPresignedUrl(
+        todoId: string): 
+        Promise<string> {
+        console.log
+            ("Generating an upload URL");
+
+        const url = this.s3Client.getSignedUrl('putObject', {Bucket: this.s3BucketName,Key: todoId,Expires: 1000,});
+        console.log(url);
+
+        return url as string;
+    }
+}
---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'jep18tz8ba';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-z5d8s9rj.us.auth0.com',
+  clientId: 'EjOPPlkDgwIFIXv2yfeg7CRy02cDnMcz',
+  callbackUrl: 'http://localhost:3000/callback'
+};
